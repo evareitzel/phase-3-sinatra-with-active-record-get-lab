d6440c30daf30ee76e7df931b43e682921c7b2d1
Eva Reitzel <evareitzel@gmail.com>
2022-12-14 15:24:09 -0500

    commit tree 5511af4312662fd27c43c1cf6a050b4d02138796

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,37 @@
 class ApplicationController < Sinatra::Base
+  set :default_content_type, 'application/json'
 
   # add routes
+  get'/bakeries' do
+    "Hello World"
+    # get all the bakeries from the database 
+    bakeries = Bakery.all
+    # send them back as a JSON array 
+    bakeries.to_json
+  end
+
+  get '/bakeries/:id' do
+ # look up the bakery in the database using its ID
+    bakery = Bakery.find(params[:id])
+    # return as JSON arr w baked goods nested arr
+    # include baked goods as a nested arr
+
+    bakery.to_json(include: :baked_goods)
+  end
+
+
+  get '/baked_goods/by_price' do
+    # get baked goods, sort by price
+    baked_goods = BakedGood.all.order(:price).reverse
+    # return as JSON arr 
+    baked_goods.to_json
+  end
+
+  get '/baked_goods/most_expensive' do
+    # get baked goods, sort by price, get most expensive baked good
+    most_expensive_baked_good = BakedGood.all.order(:price).last
+    # return as JSON arr 
+    most_expensive_baked_good.to_json
+  end
 
 end
